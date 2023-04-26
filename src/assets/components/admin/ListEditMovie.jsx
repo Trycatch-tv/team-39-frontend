d3565fecb8f51b88f364ea808cf4f49ea8427708
SebasJS <gomezvargas97@gmail.com>
2023-04-26 06:54:57 -0500

    Maquetación vista admin - Navbar y Search

---
+++ src/assets/components/admin/ListEditMovie.jsx
@@ -0,0 +1,18 @@
+
+const ListEditMovie = () => {
+  return (
+    <>
+      <div>
+        <h2>LISTADO DE PELICULAS</h2>
+        <div>
+          <h3>Avatar</h3>
+          <div>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ListEditMovie
