47b7cc6dd745e1c16ac98f6c0036c91ce775cf38
SebasJS <gomezvargas97@gmail.com>
2023-04-25 20:38:28 -0500

    configuracion eslint

---
+++ readme.md
@@ -0,0 +1,5 @@
+npm install
+
+...Configuración de eslint.
+Instalar le extension de eslint
+npm install standard -D
---
+++ src/assets/components/admin/list-movies/ListMovies.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+useState
+export const ListMovies = () => {
+return (
+        <div>ListMovies</div>  
+    )
+}
